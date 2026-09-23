fc2d2fe30b15ccc1b2a3e262977318712576e74d
adityalochan <aditya.lochan25@gmail.com>
2026-09-23 16:33:57 -0400

    resolved edge cases

--- deliveryFeeCalculator.py
+++ deliveryFeeCalculator.py
@@ -9,13 +9,13 @@
 # – Use variable names that describe what they hold, and comments marking the sections of your program:
 # reading and validating each value, and combining them
 
-order_total = float(input('Enter the order total in dollars: E.g. 25'))
-delivery_date = input('Enter the delivery day (e.g. Saturday):')
+order_total = float(input('Enter the order total in dollars: E.g. 25 :: '))
+delivery_date = input('Enter the delivery day (e.g. Saturday) :: ').lower()
 
 if not (0 <= order_total <= 1000):
     print('Order total must be between 0 and 1000')
 
-if delivery_date not in ['saturday', 'sunday']:
+if delivery_date not in ['monday', 'tuesday', 'wednesday', 'thursday', 'friday','saturday', 'sunday']:
     print('Delivery date must be Saturday or Sunday')
     exit()
 
@@ -32,9 +32,14 @@ elif order_total < 100:
     delivery_fee = 7.99*surcharge
 elif order_total < 250:
     delivery_fee = 5.99*surcharge
+elif order_total < 500:
+    delivery_fee = 3.99*surcharge
+elif order_total < 1000:
+    delivery_fee = 1.99*surcharge
 else:
     print('Order total must be between 0 and 1000 and delivery date must be Saturday or Sunday')
     exit()
 
+delivery_fee = round(delivery_fee, 2)
 
-print(f'Order: ${order_total} on {delivery_date }. Delivery fee: ${delivery_fee}. Weekend small-order surcharge applies.')
+print(f'Order: ${order_total} on {delivery_date}. Delivery fee: ${delivery_fee}. Weekend small-order surcharge applies.')
